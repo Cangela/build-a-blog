dbb6250b649d9ba703b757128f50bf5be5cfa2dd
Angela Tibbs <adtibbs@gmail.com>
2017-02-11 09:20:23 -0600

    set up blog so that new post for and post listing are on the same page

--- main.py
+++ main.py
@@ -1,25 +1,57 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
+import os
+import jinja2
 
-class MainHandler(webapp2.RequestHandler):
+from google.appengine.ext import db
+
+# set up jinja
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                                autoescape = True)
+
+class  Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+
+class Entry(db.Model):
+    title = db.StringProperty(required = True)
+    entry = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+class MainPage(Handler):
+    def render_new(self, title="", entry="", error=""):
+        entries = db.GqlQuery("SELECT * FROM Entry "
+                            "ORDER BY created DESC "
+                            "LIMIT 5")
+                            
+        self.render("new_post.html", title=title, entry=entry, error=error, entries=entries)
+
     def get(self):
-        self.response.write('Hello world!')
+        self.render_new()
+
+    def post(self):
+        title = self.request.get("title")
+        entry = self.request.get("entry")
+
+        if title and entry:
+            p = Entry(title = title, entry = entry)
+            p.put()
+
+            self.redirect("/")
+
+        else:
+            error = "We need both a title and some content!"
+            self.render_new(title, entry, error)
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainPage)
 ], debug=True)
