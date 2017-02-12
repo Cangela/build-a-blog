701566a1dfb023f5780ca2c24b6580a6abbc78b9
Angela Tibbs <adtibbs@gmail.com>
2017-02-12 08:05:38 -0600

    created single post display in conjunction with permalinks

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import webapp2
 import os
 import jinja2
+import re
 
 from google.appengine.ext import db
 
@@ -21,16 +22,16 @@ class  Handler(webapp2.RequestHandler):
         self.write(self.render_str(template, **kw))
 
 
-class Entry(db.Model):
+class Blog(db.Model):
     title = db.StringProperty(required = True)
-    entry = db.TextProperty(required = True)
+    blog_entry = db.TextProperty(required = True)
     created = db.DateTimeProperty(auto_now_add = True)
 
 class MainPage(Handler):
 
     #def render_main(self, entries=""):
-    def get(self, entries=""):
-        blog_entries = db.GqlQuery("SELECT * FROM Entry "
+    def get(self, blog_entries=""):
+        blog_entries = db.GqlQuery("SELECT * FROM Blog "
                         "ORDER BY created DESC "
                         "LIMIT 5")
         #t = jinja_env.get_template("mainpage.html")
@@ -39,31 +40,42 @@ class MainPage(Handler):
                         #)
         #self.response.write(content)
 
-        self.render("mainpage.html", entries=blog_entries)
+        self.render("mainpage.html", blog_entries=blog_entries)
 
 class NewPost(Handler):
-    def render_new(self, title="", entry="", error=""):
-        self.render("newpost.html", title=title,entry=entry, error=error)
+    def render_newpost(self, title="", blog_entry="", error=""):
+        self.render("newpost.html", title=title, blog_entry=blog_entry, error=error)
 
     def get(self):
-        self.render_new()
+        self.render_newpost()
 
     def post(self):
         title = self.request.get("title")
-        entry = self.request.get("entry")
+        blog_entry = self.request.get("blog_entry")
 
-        if title and entry:
-            e = Entry(title = title, entry = entry)
-            e.put()
+        if title and blog_entry:
+            b = Blog(title = title, blog_entry = blog_entry)
+            b.put()
             self.redirect("/")
 
         else:
             error = "We need both a title and some content!"
-            self.render_new(title, entry, error)
+            self.render_newpost(title, blog_entry, error)
+
+class ViewPostHandler(Handler):
+    def get(self, id):
+
+        blog_entry = Blog.get_by_id(int(id), parent=None)
+        error =""
+        if not blog_entry:
+            error = "Incorrect blog id!"
+        return self.render("singlepost.html", blog_entry=blog_entry, error=error)
+
 
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/blog', MainPage),
-    ('/newpost', NewPost)
+    ('/newpost', NewPost),
+    webapp2.Route('/blog/<id:\d+>', ViewPostHandler)
 ], debug=True)
